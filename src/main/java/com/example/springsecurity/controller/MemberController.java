f30ca163c497bcc40d1369b6ba1d62645cd4b568
jonghunbaek <qor6028@naver.com>
2023-12-14 18:51:35 +0900

    refactor : 유저 정보 검증 서비스 레이어에서 수행하도록 변경 및 jwt토큰 반환 추가

--- src/main/java/com/example/springsecurity/controller/MemberController.java
+++ src/main/java/com/example/springsecurity/controller/MemberController.java
@@ -2,16 +2,10 @@ package com.example.springsecurity.controller;
 
 import com.example.springsecurity.dto.SignInRequest;
 import com.example.springsecurity.dto.SignUpRequest;
-import com.example.springsecurity.entity.Member;
-import com.example.springsecurity.repo.MemberRepository;
 import com.example.springsecurity.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,17 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MemberController {
 
-    private final AuthenticationManager authenticationManager;
     private final MemberService memberService;
 
     @PostMapping("/sign-in")
-    public ResponseEntity<String> authenticateUser(@RequestBody SignInRequest signInRequest) {
-        Authentication authenticationRequest = UsernamePasswordAuthenticationToken.unauthenticated(signInRequest.getEmail(), signInRequest.getPassword());
-        Authentication authenticateResponse = authenticationManager.authenticate(authenticationRequest);
-
-        SecurityContextHolder.getContext().setAuthentication(authenticateResponse);
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        return new ResponseEntity<>("로그인 성공", HttpStatus.OK);
+    public String authenticateUser(@RequestBody SignInRequest signInRequest) {
+        return memberService.signIn(signInRequest);
     }
 
     @PostMapping("/sign-up")
--- src/main/java/com/example/springsecurity/service/MemberService.java
+++ src/main/java/com/example/springsecurity/service/MemberService.java
@@ -1,9 +1,14 @@
 package com.example.springsecurity.service;
 
+import com.example.springsecurity.config.security.TokenProvider;
+import com.example.springsecurity.dto.SignInRequest;
 import com.example.springsecurity.dto.SignUpRequest;
 import com.example.springsecurity.entity.Member;
 import com.example.springsecurity.repo.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -12,7 +17,10 @@ import org.springframework.stereotype.Service;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final TokenProvider tokenProvider;
 
     public void signUp(SignUpRequest signUpRequest) {
         Member member = Member.builder()
@@ -22,5 +30,12 @@ public class MemberService {
             .build();
 
         memberRepository.save(member);
+    }
+
+    public String signIn(SignInRequest signInRequest) {
+        Authentication authenticationRequest = UsernamePasswordAuthenticationToken.unauthenticated(signInRequest.getEmail(), signInRequest.getPassword());
+        authenticationManager.authenticate(authenticationRequest);
+
+        return tokenProvider.createToken(signInRequest.getEmail());
     }
 }
